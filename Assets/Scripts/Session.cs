7a31e01474665fa2caaef40b03546df54ca32126
mgackowski <mwgackowski@gmail.com>
2021-03-03 22:01:57 +0000

    Began on persistent session object

---
+++ Assets/Scripts/Session.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session : MonoBehaviour
+{
+    public string playerName = "Player";
+    // store current dice collection
+    //s store current game piece collection
+    public int levelsCompleted = 0;
+    public int polybagTokens = 0;
+    public bool boxObtained = false;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+}
