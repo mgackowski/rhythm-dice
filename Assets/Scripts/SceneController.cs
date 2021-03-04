b59dc4afdbe02832fbc422c563c1a51f15bd0e1e
mgackowski <mwgackowski@gmail.com>
2021-03-04 21:56:57 +0000

    Change scene with fade transition

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -25,20 +24,38 @@ public class SceneController : MonoBehaviour
 
     private void Start()
     {
-        //FadeOut(3f);
+        //StartCoroutine(ChangeScene("TestScene",true,2f));
+    }
+
+    public IEnumerator ChangeScene(string nextScene, bool fade = true, float transitionDuration = 0.5f)
+    {
+        if(fade)
+        {
+            FadeOut(transitionDuration / 2);
+            yield return StartCoroutine(Fade((transitionDuration / 2), 0f, 1f));
+        }
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
+        if(fade)
+        {
+            while(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name != nextScene)
+            {
+                yield return null;
+            }
+            yield return StartCoroutine(Fade((transitionDuration / 2), 1f, 0f));
+        }
     }
 
     public void FadeOut(float duration)
     {
-        StartCoroutine(Fade(duration, 1f));
+        StartCoroutine(Fade(duration, 0f, 1f));
     }
 
     public void FadeIn(float duration)
     {
-        StartCoroutine(Fade(duration, 0f));
+        StartCoroutine(Fade(duration, 1f, 0f));
     }
 
-    private IEnumerator Fade(float duration, float targetAlpha)
+    private IEnumerator Fade(float duration, float firstAlpha, float targetAlpha)
     {
         GameObject fadeScreenObject = GameObject.FindGameObjectWithTag("FadeScreen");
         if (!fadeScreenObject)
@@ -51,8 +68,9 @@ public class SceneController : MonoBehaviour
 
         float elapsedTime = 0f;
         Color newColour = fadeScreen.color;
-        float alpha = newColour.a;
-
+        newColour.a = firstAlpha;
+        fadeScreen.color = newColour;
+        float alpha = firstAlpha;
         while (elapsedTime <= duration)
         {
             newColour.a = Mathf.Lerp(alpha, targetAlpha, elapsedTime / duration);
